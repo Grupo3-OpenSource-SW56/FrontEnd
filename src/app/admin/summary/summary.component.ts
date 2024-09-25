1250af3607184ee11c30fedf85ce0c3cb461743c
Rodrigojsr12 <166780370+Rodrigojsr12@users.noreply.github.com>
2024-09-25 13:30:43 -0500

    Add files via upload

---
+++ src/app/admin/summary/summary.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-summary',
+  templateUrl: './summary.component.html',
+  styleUrl: './summary.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SummaryComponent {}
